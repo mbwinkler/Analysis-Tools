03842a4046b1e6c2b4f24115e8f68ac3ba850e2d
Michael <Winkler.B.Michael@gmail.com>
2022-04-07 00:43:47 +0200

    Hysteresenplotter File angelegt und TODO erstellt

---
+++ Library/src/Plotting/PlotHystereses.py
@@ -0,0 +1,3 @@
+#TODO: Plotter für Hysteresen aus Rohdaten
+#   Parameter: Cycles = Array(0.1,0.5,0.9) --> plottet 3 mal bei 0.1,0.5 und 0.9 der max Zyklen
+#   Restliche Plotting Parameter wie Style etc einfach übernehmen
