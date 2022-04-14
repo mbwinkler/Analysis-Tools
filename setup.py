226c52cd825d157e85c6c2614958d7736ff78398
Michael Winkler <Winkler.B.Michael@gmail.com>
2022-04-14 14:34:14 +0200

    0.0.11 released

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TUMMET",
-    version="0.0.10",
+    version="0.0.11",
     author="Michael Winkler",
     author_email="michael.b.winkler@tum.de",
     description="A packages to evaluate material test data",
